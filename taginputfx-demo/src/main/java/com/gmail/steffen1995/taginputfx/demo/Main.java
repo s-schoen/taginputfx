14d51adf7562a3ff479611029865c22846089168
Steffen <steffen1995@gmail.com>
2020-09-23 10:42:30 +0200

    initial commit

---
+++ taginputfx-demo/src/main/java/com/gmail/steffen1995/taginputfx/demo/Main.java
@@ -0,0 +1,7 @@
+package com.gmail.steffen1995.taginputfx.demo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ taginputfx/src/main/java/com/gmail/steffen1995/taginputfx/TagInput.java
@@ -0,0 +1,4 @@
+package com.gmail.steffen1995.taginputfx;
+
+public class TagInput {
+}
